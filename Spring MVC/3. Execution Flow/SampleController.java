ea795ba0a2aa80d697560fbef1dc01e27f844c76
Daniel <byoung.mokk@gmail.com>
2022-11-29 21:50:56 +0900

    SampleController.java Update

--- Spring MVC/3. Execution Flow/SampleController.java
+++ Spring MVC/3. Execution Flow/SampleController.java
@@ -22,29 +22,29 @@ public class SampleController00 {
 
     // 6. .jsp 호출 메커니즘을 알아보자
     /*
-    컨트롤러의 핸들러는 아래의 역할 및 값을 반환
-    1. 역할 : 요청을 위임받아 처리
-    2. 뷰의 이름을 반환
-    3. 역할에 따라, 요청을 위임처리한 결과 => Model(비즈니스데이터 / Model = 상자) 생성 및 View 로 전달
+        컨트롤러의 핸들러는 아래의 역할 및 값을 반환
+        1. 역할 : 요청을 위임받아 처리
+        2. 뷰의 이름을 반환
+        3. 역할에 따라, 요청을 위임처리한 결과 => Model(비즈니스데이터 / Model = 상자) 생성 및 View 로 전달
     */
     @GetMapping("/ex01")
     // == @RequestMapping(path = "/basicOnlyGet", method = RequestMethod.GET)
     public String ex01(ParamsDTO dto) {
         // DispatcherServlet 에게, 2개의 전송파라미터를 이 매개변수에 넣어주세요! 라고 부탁
         /*
-        DispatcherServlet 의 특별한 역할
-            1. 모든 전송 파라미터를 수집해 놓는다. 전달가능
-            2. 타입까지 맞춰서 전달가능
-            3. 전송파라미터의 이름을 맞추지 않으면 500번대 에러
-            4. DTO 클래스의 필드에 전송파라미터 넣어줌 (필드의 이름 == 전송파라미터의 이름)
+            DispatcherServlet 의 특별한 역할
+                1. 모든 전송 파라미터를 수집해 놓는다. 전달가능
+                2. 타입까지 맞춰서 전달가능
+                3. 전송파라미터의 이름을 맞추지 않으면 500번대 에러
+                4. DTO 클래스의 필드에 전송파라미터 넣어줌 (필드의 이름 == 전송파라미터의 이름)
         */
         log.debug("ex01({}) invoked.", dto);
 
         return "ex01";
          /*
-         이게 바로 MVC 패턴에서 View 의 이름
-         BY View Resolver Configuration
-         /WEB_INF/views/ + ex01 + .jsp == /WEB_INF/views/ex01.jsp
+            이게 바로 MVC 패턴에서 View 의 이름
+            BY View Resolver Configuration
+            /WEB_INF/views/ + ex01 + .jsp == /WEB_INF/views/ex01.jsp
          */
     }
 
@@ -68,9 +68,9 @@ public class SampleController00 {
 
     // 8.with multi-values request parameters
     /*
-    체크박스 처럼 값이 여러개가 날아오면 어떻게 처리할까? ex) String[] hobby = req.getParameterValues("hobby");
-    아래의 예제를 통해 알아보자
-    가장 간단하고 확실한 방법은 배열을 쓰자
+        체크박스 처럼 값이 여러개가 날아오면 어떻게 처리할까? ex) String[] hobby = req.getParameterValues("hobby");
+        아래의 예제를 통해 알아보자
+        가장 간단하고 확실한 방법은 배열을 쓰자
     */
     @GetMapping("/ex03")
 //    public String ex03List(List<String> hobby)  {                                // XX
@@ -93,10 +93,10 @@ public class SampleController00 {
     // 전송파라미터의 값이 날짜포맷형식(String)으로 전달될 때,
     // 컨트롤러의 핸들러 메소드의 매개변수로 아예 처음부터, Data 객체로 얻을 수 있다.
     /*
-    입자일자를 전송한다고 하자 매개변수의 타입으로 Date 로 받을 수 있을까? XX
-    @DateTimeFormat 을 붙여보자
-	- 날짜형식의 String 타입을 Date 타입으로 변환시켜준다. yyyy-MM-dd 형태로 들어왔다는걸 알려주면...속성인 (pattern="")에게
-	- 여러 형식이 있으니 상황에 맞게 찾아 쓰면 될 것 같다.
+        입자일자를 전송한다고 하자 매개변수의 타입으로 Date 로 받을 수 있을까? XX
+        @DateTimeFormat 을 붙여보자
+        - 날짜형식의 String 타입을 Date 타입으로 변환시켜준다. yyyy-MM-dd 형태로 들어왔다는걸 알려주면...속성인 (pattern="")에게
+        - 여러 형식이 있으니 상황에 맞게 찾아 쓰면 될 것 같다.
     */
     @GetMapping("/ex04")
     public String ex04( @DateTimeFormat(pattern = "yyyy-MM-dd") Date hireDate) {
@@ -110,12 +110,12 @@ public class SampleController00 {
     // 10. Predefined Model parameter
     // to transfer request parameter into the View
     /*
-    Model 을 이용한 예제 DTO 객체 만들어 실습 ( SampleDTO.java )
-    Model 객체는 직접 만들지 않고 요청한다 to DispatcherServlet (매개변수 자리에서...)
-    로직을 수행한 결과 데이터가 안에 들어간다. 실제타입은 Map 객체 키,값 형태로 들어간다.
-    RequestScope 에 바인딩되는 결과
-    model 객체 안에 객체를 넣는 순간 그 객체는 공유속성이 되며 RequestScope 에 바인딩된다.
-    .jsp 파일에서 끄집어내보자
+        Model 을 이용한 예제 DTO 객체 만들어 실습 ( SampleDTO.java )
+        Model 객체는 직접 만들지 않고 요청한다 to DispatcherServlet (매개변수 자리에서...)
+        로직을 수행한 결과 데이터가 안에 들어간다. 실제타입은 Map 객체 키,값 형태로 들어간다.
+        RequestScope 에 바인딩되는 결과
+        model 객체 안에 객체를 넣는 순간 그 객체는 공유속성이 되며 RequestScope 에 바인딩된다.
+        .jsp 파일에서 끄집어내보자
     */
     @GetMapping("/ex05")
     public String ex05(String name, Integer age, Integer page, Model model) {
@@ -136,18 +136,18 @@ public class SampleController00 {
 
         return "commandObject";
         /*
-        Spring MVC 의 Controller 의 Handler Method 에서, 전송파라미터들을 수집하는 용도로 지정한 객체(DTO)를
-            ==> Spring 에서는 Command Object 라고 부름
-            ==> Command Object 의 특징 : MVC 패턴에서 마지막에 호출될 View(즉, .jsp)까지 자동으로 전달됨*** Model 데이터와 함께...
+            Spring MVC 의 Controller 의 Handler Method 에서, 전송파라미터들을 수집하는 용도로 지정한 객체(DTO)를
+                ==> Spring 에서는 Command Object 라고 부름
+                ==> Command Object 의 특징 : MVC 패턴에서 마지막에 호출될 View(즉, .jsp)까지 자동으로 전달됨*** Model 데이터와 함께...
         */
     }
 
 
     // 11. 10번예제 응용
     /*
-    jsp 파일에서 출력될 때 10번과는 EL 표현식이 좀 다른것 같다.
-    10번 : ${SampleDTO}
-    11번 : ${sampleDTO} => 첫문자 소문자
+        jsp 파일에서 출력될 때 10번과는 EL 표현식이 좀 다른것 같다.
+        10번 : ${SampleDTO}
+        11번 : ${sampleDTO} => 첫문자 소문자
     */
     @GetMapping("/ex06")
     public String ex05( SampleDTO dto, Integer page, Model model ) {
