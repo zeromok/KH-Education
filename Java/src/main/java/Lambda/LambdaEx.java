8917dfd197c268a7fad869f7df339bbfa7382e1b
zeromok <byoung.mokk@gmail.com>
2022-12-15 19:58:25 +0900

    Lambda Update

--- Java/src/main/java/Lambda/LambdaEx.java
+++ Java/src/main/java/Lambda/LambdaEx.java
@@ -19,6 +19,34 @@ public class LambdaEx {
         // 람다식으로 추상메소드를 구현
         task.run();
 
+        /*
+            Runnable task = null;
+
+            task = new Runnable() {
+                @Override
+                public void run() {
+                    ;;
+                }
+            }; // 익명구현객체 코딩기법 -> "익명구현객체"의 생성
+        */
+
     } // main()
 
 } // end class
+
+@FunctionalInterface
+interface OurInterface {
+
+    // 정적 final 변수
+    public static final double PI = 3.14159;
+
+    // 기본메소드
+    public default void defaultMethod() {;;}
+
+    // 정적메소드
+    public static void staticMethod() {;;}
+
+    // 추상메소드
+    public abstract void abstractMethod();
+
+} // end Interface
